4fee48739d9d53350da606faeb8f4d19280ed6f3
JC <jcbrady89@yahoo.com>
2020-09-04 09:39:10 -0600

    Updated
    Changed to javascript instead of autofocus.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-// uncomment to use javascript instead of autofocus in html
-// document.getElementById("name").focus()
+// use javascript instead of autofocus in html to select the first form field on page load.
+document.getElementById("name").focus()
 
+// *This is the only part of the project where index.html needs to be changed.
 // initially hide the "other" input for job roles
 const otherJob = document.getElementById("other-title")
 // otherJob.style.display = "hidden" doesn't work!!!
