5ea0504d6936296056836a6efe196c8bf13fd03d
JC <jcbrady89@yahoo.com>
2020-09-06 12:10:50 -0600

    T-Shirt section
    e.target and working with an issue in the if statement.

--- js/script.js
+++ js/script.js
@@ -11,4 +11,36 @@ if (false) {
   otherJob.setAttribute("type", "text")
 }
 
-console.log(otherJob)
+// NEXT SECTION - SELECT T-SHIRT'S
+
+const selectDesign = document.getElementById("design")
+selectDesign.firstElementChild.innerHTML = "Please Select a T-shirt Theme"
+
+const selectColor = document.getElementById("color")
+selectColor.style.display = "none"
+// document.getElementById("color").style.visibility = "hidden"
+
+// hide label for Colors
+const shirtColorDiv = document.getElementById("shirt-colors")
+shirtColorDiv.firstElementChild.style.display = "none"
+
+// eventlisteners on select elements
+
+selectDesign.addEventListener("change", e => {
+  // show label for Colors
+  shirtColorDiv.firstElementChild.style.display = "block"
+  // show color select menu
+  selectColor.style.display = "block"
+
+  let changed = e.target
+  // console.log(changed.options[0].value) // logs "please select a t-shirt theme"
+  console.log(changed.options[1].value) // logs "js puns"
+  console.log(changed.options[2].value) // logs "heart js"
+
+  // conditional NOT WORKING with these values
+  if (changed.options[1].value === "js puns") {
+    console.log("JS Puns was selected")
+  } else if (changed.options[2].value === "heart js") {
+    console.log("I love Javascript was selected")
+  }
+})
