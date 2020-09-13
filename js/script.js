b04aa1d176bf8e5550942cefcdb40d7a2b631936
JC <jcbrady89@yahoo.com>
2020-09-13 08:54:06 -0600

    Share on Slack
    Share on Slack

--- js/script.js
+++ js/script.js
@@ -85,10 +85,14 @@ activity.appendChild(costElement)
 // Add a variable to reference the DOM `input` element that was just checked
 // Add a cost variable on data-cost attribute to get the cost
 // If checked add to totalCost and if unchecked subtract from totalCost
+// Add a variable to get info from the data-day-and-time attribute
 
 activity.addEventListener("change", function (e) {
   let check = e.target
   let cost = check.getAttribute("data-cost")
+  let dayAndTime = check.getAttribute("data-day-and-time")
+  let activityCheckboxes = document.querySelectorAll('input[type="checkbox"]')
+
   cost = parseInt(cost)
 
   // console.log(parseInt(cost))
@@ -98,12 +102,51 @@ activity.addEventListener("change", function (e) {
     //console.log(check.getAttribute("name"))
     //console.log("is now checked")
     totalCost = totalCost + cost
-    console.log(totalCost)
+    // console.log(totalCost)
   } else if (check.checked === false) {
     //console.log(check.getAttribute("name"))
     //console.log("is now unchecked")
     totalCost = totalCost - cost
-    console.log(totalCost)
+    //console.log(totalCost)
   }
-  costElement.textContent = "Total: $ " + totalCost
+  costElement.textContent = "Total: $" + totalCost
+
+  // ● When an activity is checked, disable any activity that occurs at the same day and time
+  // (i.e. "conflicting activities") without disabling the activity that was just checked.
+  // ● And when an activity is unchecked, you want to enable any conflicting activities.
+  // To do this, you’ll need to loop over all the checkbox inputs in the Activity section. It will be
+  // helpful to create a variable that targets the activity input element at the current iteration of the
+  // loop. Remember, you do this with bracket notation, using the loop iterator in the brackets.
+  // Something like this: `input[i]`. Be sure to log out the variable you just created to test its value.
+
+  // ● First, does the activity occur at the same day and time as the activity that was just
+  // clicked? We can check this by seeing if the activity in the current loop iteration has a
+  // `data-day-and-time` attribute that is equal to the `data-day-and-time` attribute of the
+  // element that was just clicked.
+  // ● Second, is the activity different than the activity that was just clicked? We can check
+  // this by seeing if the activity that was just clicked is not equal to the activity in the
+  // current loop iteration.
+  // Both of these conditions should be checked in a single if statement using the `&&` operator.
+  // If both conditions evaluate to "true", then this activity needs to be disabled or enabled
+  // depending on whether the clicked activity was checked or unchecked. An `if/else` statement
+  // will help here:
+
+  // console.log(check) //  the input element that was clicked (checked or unchecked)
+  console.log(dayAndTime) // check's day and time data-attribute
+  // console.log(activityCheckboxes) // the nodelist
+
+  for (let i = 0; i < activityCheckboxes.length; i++) {
+    activityCheckboxes[i] // individual inputs
+    // variable to get the current loop element's data-day-and-time attribute
+    let inputAttrubute = activityCheckboxes[i].getAttribute("data-day-and-time")
+
+    if (dayAndTime === inputAttrubute && check !== activityCheckboxes[i]) {
+      console.log("ALERT: conflicting schedule: " + dayAndTime + " IS AT THE SAME TIME AS " + inputAttrubute)
+      // check.disabled = false
+      activityCheckboxes[i].disabled = true
+    } else {
+      console.log('test the "if" condition with the else statement')
+      // activityCheckboxes[i].disabled = false
+    }
+  }
 })
